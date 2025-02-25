6de0da7bb32257d33a9c7e2e20c4d1f1386940d3
Guilherme Sadovski <guilhermesadovski@gmail.com>
2025-02-24 23:16:12 -0300

    config: customized mathjax with introduction of $$, \tr, \slashed;

--- assets/js/mathjax-setup.js
+++ assets/js/mathjax-setup.js
@@ -1,5 +1,13 @@
 window.MathJax = {
   tex: {
     tags: "ams",
+          autoload: {
+            centernot: ['centernot']
+          },
+          inlineMath: [['$', '$']],
+          macros: {
+            tr: "{\\mathrm{tr}}",
+            slashed: ["{\\centernot#1}", 1],
+          },
   },
 };
